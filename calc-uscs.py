56e2f88aab5b070bec847548cf1d3697694f7820
Arthur Luciano Casal <casalarthur00@gmail.com>
2026-08-20 13:08:47 -0300

    Add files via upload
    Calculadora de média acadêmica desenvolvida em Python para calcular a média final com base nas notas da N1, AI e atividades (AFs), considerando de 0 a 4 atividades. Projeto desenvolvido para praticar estruturas condicionais, entrada de dados, operações matemáticas e formatação de saída.

---
+++ calc-uscs.py
@@ -0,0 +1,63 @@
+afs = int(input("Quantas AF's voce teve? [0 a 4] "))
+n1 = float(input('Qual sua nota na N1? '))
+ai = float(input('Qual sua nota na A1? '))
+
+if afs == 1:
+    nota_af1 = float(input('Qual foi sua nota da AF 1 '))
+    media = (n1 + ((ai + nota_af1) / 2)) / 2
+    print(f"""===============================
+    {'Boletim':^20}
+
+Notas AF:    {nota_af1:.2f}
+Nota N1:     {n1:.2f}
+Nota AI:     {ai:.2f}
+Media N2:    {(ai + nota_af1)/2:.2f}
+Media final: {media:.2f}
+===============================  """)
+elif afs == 2:
+    nota_af1 = float(input('Qual foi sua nota da AF 1 '))
+    nota_af2 = float(input('Qual foi sua nota da AF 2 '))
+    media = (n1 + ((ai + ((nota_af1 + nota_af2)/2)) / 2)) / 2
+    print(f"""===============================
+    {'Boletim':^20}
+
+Notas AF:    {(nota_af1 + nota_af2) / 2:.2f}
+Nota N1:     {n1:.2f}
+Nota AI:     {ai:.2f}
+Media N2:    {(ai + ((nota_af1 + nota_af2)/ 2))/2:.2f}
+Media final: {media:.2f}
+===============================  """)
+elif afs == 3:
+    nota_af1 = float(input('Qual foi sua nota da AF 1 '))
+    nota_af2 = float(input('Qual foi sua nota da AF 2 '))
+    nota_af3 = float(input('Qual foi sua nota da AF 3 '))
+    media = (n1 + ((ai + ((nota_af1 + nota_af2 + nota_af3)/3)) / 2)) / 2
+    print(f"""===============================
+    {'Boletim':^20}
+    
+Notas AF:    {(nota_af1 + nota_af2 + nota_af3) / 3:.2f}
+Nota N1:     {n1:.2f}
+Nota AI:     {ai:.2f}
+Media N2:    {(ai + ((nota_af1 + nota_af2 + nota_af3)/ 3))/2:.2f}
+Media final: {media:.2f}
+===============================  """)
+elif afs == 4:
+    nota_af1 = float(input('Qual foi sua nota da AF 1 '))
+    nota_af2 = float(input('Qual foi sua nota da AF 2 '))
+    nota_af3 = float(input('Qual foi sua nota da AF 3 '))
+    nota_af4 = float(input('Qual foi sua nota da AF 4 '))
+    media = (n1 + ((ai + ((nota_af1 + nota_af2 + nota_af3 + nota_af4)/4)) / 2)) / 2
+    print(f"""===============================
+    {'Boletim':^20}
+    
+Notas AF:    {(nota_af1 + nota_af2 + nota_af3 + nota_af4) / 4:.2f}
+Nota N1:     {n1:.2f}
+Nota AI:     {ai:.2f}
+Media N2:    {(ai + ((nota_af1 + nota_af2 + nota_af3 + nota_af4) / 4))/2:.2f}
+Media final: {media:.2f}
+===============================  """)
+elif not afs:
+    print('Voce nao digitou corretamente as AFS')
+else:
+    print('Voce nao digitou corretamente as AFS')
+
